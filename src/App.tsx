47eb4b3dac33512dbad92afe1d599fdf2d5b69ea
Igor Ulian <igorulian@outlook.com>
2021-09-04 00:55:58 -0300

    setup react project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Casamendo lincu e jesca </h1>
+      <button> comprar </button>
+      <button> comprar </button>
+      <button> comprar </button>
+      <button> comprar </button>
+    </div>
+  );
+}
+
+export default App;
