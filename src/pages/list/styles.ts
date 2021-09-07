5840b191432f4178448547c318d61ba6b7f714dc
Igor Ulian <48739190+igorulian@users.noreply.github.com>
2021-09-07 17:04:37 -0300

    fixed list page background color

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -5,7 +5,7 @@ export const ListContainer = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 100vh;
+    height: 100%;
     background-color: #fafafa;
     align-items: center;
 `
